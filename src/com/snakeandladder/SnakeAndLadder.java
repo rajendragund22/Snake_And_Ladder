f94de5260b56c340e163533348ed39b007190b81
Rajendra Gund <rajendragund22@gmail.com>
2024-01-14 19:38:21 +0530

    [Rajendra] Update : Updated the snake and ladder for check 3 option

--- src/com/snakeandladder/SnakeAndLadder.java
+++ src/com/snakeandladder/SnakeAndLadder.java
@@ -5,5 +5,26 @@ public class SnakeAndLadder {
 		System.out.println("Welcome To the snake and ladder code");
 		int position = 0;
 		System.out.println("Your Starting position is : " + position);
+		int dice = (int) Math.floor(Math.random() * 6 + 1);
+		System.out.println(dice);
+		int option = (int) Math.floor(Math.random() * 3 + 1);
+		System.out.println(option);
+		switch (option) {
+		case 1:
+			System.out.println("No Play");
+			break;
+		case 2:
+			position = position + dice;
+			System.out.println("Position is : " + position);
+			break;
+		case 3:
+			position = position - dice;
+			if (position < 0) {
+				position = 0;
+			}
+			System.out.println("Position is : " + position);
+			break;
+		}
+
 	}
 }
