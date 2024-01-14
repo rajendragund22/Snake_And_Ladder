d4f8eb5c6b868fda49c6702f9c2399c2d8a319fc
Rajendra Gund <rajendragund22@gmail.com>
2024-01-14 19:00:40 +0530

    [Rajendra] Update : Updated the snake and ladder for start position

--- src/com/snakeandladder/SnakeAndLadder.java
+++ src/com/snakeandladder/SnakeAndLadder.java
@@ -3,5 +3,7 @@ package com.snakeandladder;
 public class SnakeAndLadder {
 	public static void main(String[] args) {
 		System.out.println("Welcome To the snake and ladder code");
+		int position = 0;
+		System.out.println("Your Starting position is : " + position);
 	}
 }
